3e9c3cbaaee68ea9833764baa8af99e86677a968
daniele.adriano@gmail.com <ciapet44github>
2020-04-11 14:38:38 +0200

    timbre detail activity: added custom button bar

--- app/src/main/java/com/unimi/lim/hmi/ui/TimbreDetailActivity.java
+++ app/src/main/java/com/unimi/lim/hmi/ui/TimbreDetailActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.unimi.lim.hmi.R;
 import com.unimi.lim.hmi.ui.fragment.TimbreDetailFragment;
@@ -16,5 +17,8 @@ public class TimbreDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timbre_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_timbre_detail);
+        setSupportActionBar(toolbar);
     }
 }
