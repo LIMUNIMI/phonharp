c289aede9fced111c07bb092a2eb760540c0aae7
MotAtlas <motatlas@gmail.com>
2022-07-18 15:07:25 +0200

    LowPass filter and HighPass filter classes

--- app/src/main/cpp/Filters.h
+++ app/src/main/cpp/Filters.h
@@ -6,4 +6,35 @@ class Filter{
     virtual void resetFilter() = 0;
 };
 
+class LowPassFilter: public Filter{
+    float filterSample(float currentSample){
+        return 0;
+    }
+
+    void resetFilter(){
+        oldFilteredSample = 0.0f;
+        oldRawSample = 0.0f;
+    }
+
+protected:
+    float oldRawSample = 0.0f;
+    float oldFilteredSample = 0.0f;
+};
+
+
+class HighPassFilter: public Filter{
+    float filterSample(float currentSample){
+        return 0;
+    }
+
+    void resetFilter(){
+        oldFilteredSample = 0.0f;
+        oldRawSample = 0.0f;
+    }
+
+protected:
+    float oldRawSample = 0.0f;
+    float oldFilteredSample = 0.0f;
+};
+
 #endif //HMI_FILTERS_H
